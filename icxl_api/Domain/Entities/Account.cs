557dcf7776f17e284b8188d3b0e30ef332f2646b
icxldd <1278245922@qq.com>
2020-08-28 22:33:08 +0800

    last

--- icxl_api/Domain/Entities/Account.cs
+++ icxl_api/Domain/Entities/Account.cs
@@ -13,18 +13,15 @@ namespace icxl_api.Entities
         public string Id { get; set; }
         public string Name { get; set; }
         public string PassWord { get; set; }
-
     }
 
     public class CreateAccountDto
     {
         [Required(ErrorMessage = "ID不得为空")]
         public string Id { get; set; }
-
         [Required(ErrorMessage = "name不得为空")]
         [NoSpace(ErrorMessage = "不能包含空格")]
         public string Name { get; set; }
-
         [Required(ErrorMessage = "用户password不得为空")]
         public string PassWord { get; set; }
     }
