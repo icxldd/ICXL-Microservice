7016e0c987842cbe7b15e7632e6622dfd8ae1899
icxldd <1278245922@qq.com>
2020-09-10 21:29:20 +0800

    last1

--- Test/Program.cs
+++ Test/Program.cs
@@ -10,35 +10,35 @@ namespace Test
         {
 
             var client = new HttpClient();
-
-            //var disco = await client.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest { Address = "http://192.168.0.104:9703/auto", Policy = new DiscoveryPolicy { RequireHttps = false } });
-
-            //var disco = await client.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest { Address = "http://192.168.0.104:5000", Policy = new DiscoveryPolicy { RequireHttps = false } });
-
-            string url = "http://192.168.0.102:9703/auto/connect/token";
-            var tokenResponse = await client.RequestPasswordTokenAsync(new PasswordTokenRequest
-            {
-                //Address = disco.TokenEndpoint,
-                Address = url,
-                ClientId = "ro.client",
-                ClientSecret = "secret",
-                Scope = "api1",
-                UserName = "icxl",
-                Password = "123456",
-            });
-
-            if (tokenResponse.IsError)
-            {
-                Console.WriteLine(tokenResponse.Error);
-                return;
-            }
-
-            Console.WriteLine(tokenResponse.Json);
-
-            client.SetBearerToken(tokenResponse.AccessToken);
             while (true)
             {
-                var response = await client.GetAsync("http://192.168.0.102:9703/basic/api/Home");
+                //var disco = await client.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest { Address = "http://192.168.0.104:9703/auto", Policy = new DiscoveryPolicy { RequireHttps = false } });
+
+                //var disco = await client.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest { Address = "http://192.168.0.104:5000", Policy = new DiscoveryPolicy { RequireHttps = false } });
+
+                string url = "http://192.168.0.104:9503/auto/connect/token";
+                var tokenResponse = await client.RequestPasswordTokenAsync(new PasswordTokenRequest
+                {
+                    //Address = disco.TokenEndpoint,
+                    Address = url,
+                    ClientId = "ro.client",
+                    ClientSecret = "secret",
+                    Scope = "api1",
+                    UserName = "icxl",
+                    Password = "123456",
+                });
+
+                if (tokenResponse.IsError)
+                {
+                    Console.WriteLine(tokenResponse.Error);
+                    return;
+                }
+
+                Console.WriteLine(tokenResponse.Json);
+
+                client.SetBearerToken(tokenResponse.AccessToken);
+
+                var response = await client.GetAsync("http://192.168.0.104:9503/basic/api/Home");
                 if (!response.IsSuccessStatusCode)
                 {
                     Console.WriteLine(response.StatusCode);
